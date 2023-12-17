bffae05753c9949c32145871e6945f3b0664d89f
gryusvetlana <144324528+yulana7@users.noreply.github.com>
2023-12-17 21:32:31 +0300

    Update test_rectangle.py

--- test_rectangle.py
+++ test_rectangle.py
@@ -15,9 +15,9 @@ class RectangleTestCase(unittest.TestCase):
     def test_perimeter(self):
         res = perimeter(5, 3)
         self.assertEqual(res, 16)
-    # def test_zero_perimeter(self):
-    #     res = perimeter(0, 10)
-    #     self.assertEqual(res, 'incorrect input')
+    def test_zero_perimeter(self):
+        res = perimeter(0, 10)
+        self.assertRaises(Exception, res, 'incorrect input')
     def test_float_perimeter(self):
         res = perimeter(7.7, 10.2)
         self.assertEqual(res, 35.8)
